b2c17919074232cc267ed3819ed421a2d566b5fa
lkq <1640759631@qq.com>
2023-04-24 17:43:09 +0800

    第五次提交

--- src/main/java/com/example/springboot/entity/Menu.java
+++ src/main/java/com/example/springboot/entity/Menu.java
@@ -32,4 +32,6 @@ public class Menu {
     private List<Menu> children;
 
     private Integer pid;
+
+    private String pagePath;
 }
--- src/main/java/com/example/springboot/service/RoleService.java
+++ src/main/java/com/example/springboot/service/RoleService.java
@@ -1,6 +1,7 @@
 package com.example.springboot.service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.springboot.entity.Menu;
 import com.example.springboot.entity.Role;
 import com.example.springboot.entity.RoleMenu;
 import com.example.springboot.mapper.RoleMapper;
@@ -16,18 +17,24 @@ public class RoleService extends ServiceImpl<RoleMapper, Role> {
 
     @Resource
     private RoleMenuMapper roleMenuMapper;
+    @Resource
+    private MenuService menuService;
 
     @Transactional
     public void setRoleMenu(Integer roleId, List<Integer> menuIds) {
-//        QueryWrapper<RoleMenu> queryWrapper = new QueryWrapper<>();
-//        queryWrapper.eq("role_id",roleId);
-//        roleMenuMapper.delete(queryWrapper);
-
         // 先删除当前角色id所有的绑定关系
         roleMenuMapper.deleteByRoleId(roleId);
 
         // 再把前端穿过来的绑定菜单id数组绑定到当前的这个角色id上去
         for (Integer menuId : menuIds) {
+            Menu menu = menuService.getById(menuId);
+            if(menu.getPid() != null && !menuIds.contains(menu.getPid())){   // 二级菜单 并且传过来的menuIds数组没有它的父级id
+                // 那么我们就补上这个父级id
+                RoleMenu roleMenu = new RoleMenu();
+                roleMenu.setRoleId(roleId);
+                roleMenu.setMenuId(menu.getPid());
+                roleMenuMapper.insert(roleMenu);
+            }
             RoleMenu roleMenu = new RoleMenu();
             roleMenu.setRoleId(roleId);
             roleMenu.setMenuId(menuId);
